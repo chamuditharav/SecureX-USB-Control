de6f42c6c63f0ba65995669be29cf3cda954d30b
chamuditharav <chamuditharavindu.macr@gmail.com>
2022-10-22 14:58:53 +0530

    USB check daemon fix

--- Legacy/disTest.py
+++ Legacy/disTest.py
@@ -7,17 +7,32 @@ import win32ui
 
 import win32com.client
 
-wmi = win32com.client.GetObject ("winmgmts:")
-for usb in wmi.InstancesOf ("Win32_USBHub"):
+
+
+import wmi
+
+def getLetter(deviceID):
+    logical_disk = wmi.WMI().query("SELECT * FROM Win32_LogicalDisk WHERE DeviceID=USB\\VID_0718&PID_0638\\07A10603AD5E64C2")
+    print('Drive letter: {}'.format(logical_disk.DeviceID))
+
+
+
+
+
+
+
+
+#wmi = win32com.client.GetObject ("winmgmts:")
+for usb in win32com.client.GetObject ("winmgmts:").InstancesOf ("Win32_USBHub"):
     # print usb.DeviceID
     # pprint.pprint (dir(usb))
     # pprint.pprint (vars(usb))
     # print usb.__dict__
 
+
     # print ('Device ID:', usb.DeviceID)
     # print ('Name:', usb.name)
     # print ('System Name:', usb.SystemName)
-    # print ('Caption:', usb.Caption)
     # print ('Caption:', usb.Caption)
     # print ('ClassCode:', usb.ClassCode)
     # print ('CreationClassName:', usb.CreationClassName)
@@ -25,9 +40,10 @@ for usb in wmi.InstancesOf ("Win32_USBHub"):
     # print ('Description:', usb.Description)
     # print ('PNPDeviceID:', usb.PNPDeviceID)
     # print ('Status:', usb.Status)
+    # print(usb.CurrentConfigValue)
+    
     # print ('\n')
     pass
-
 # Fetches the list of all usb devices:
 
 
@@ -44,8 +60,11 @@ for usb in wmi.InstancesOf ("Win32_USBHub"):
 
 # proc = subprocess.run(["devcon", 'remove', "USB\\VID_0718&PID_0638"]) #USB\VID_0EA0&PID_2168
 #proc = subprocess.run(["devcon", 'remove', "USB\\VID_0718&PID_0638"], stderr=subprocess.PIPE, stdout=subprocess.PIPE) #USB\VID_0EA0&PID_2168
-time.sleep(2)
 
+
+
+"""
+time.sleep(2)
 proc = proc = subprocess.run(["devcon", 'remove', "USB\\VID_0718&PID_0638"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
 time.sleep(2)
@@ -58,6 +77,12 @@ print("1 device(s) were removed" in out)
 #os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
 
 time.sleep(2)
+
+#./devcon.exe remove "USB\VID_0718&PID_0638"
+
+
+"""
+
 
 #subprocess.run(['devcon', 'enable', "USB\\VID_0EA0&PID_2168"])
 
@@ -78,3 +103,4 @@ if process_exists('CalculatorApp.exe'):
 else:
     print("84855151")
     '''
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from lib import agent, genDevcon
 from hashlib import sha256
 
 
+
 def devconIntegrity():
     if(not (os.path.exists("lib/devcon.exe"))):
         devconMake = open("lib/devcon.exe",'wb')
@@ -20,23 +21,15 @@ def devconIntegrity():
             devconMake = open("lib/devcon.exe",'wb')
             devconMake.write(bytes.fromhex(genDevcon.devcon_bkp))
             devconMake.close()
-        # else:
-        #     devcon = f"{os.getcwd()}\lib\devcon"
-        #     agent.usbWatchdog_service(devcon)
+
 
 if __name__ == "__main__":
 
-    try:
 
+    try:
         devconIntegrity()
         devcon = f"{os.getcwd()}\lib\devcon"
-        agent.usbWatchdog_service(devcon,0.1)
-    
+        agent.usbWatchdog_service(devcon,0.01)
     except:
         agent.pushLog("Program exec error")
 
-
-    #file = open("lib/devcon.exe",'rb')
-    #content = file.read()
-
-    #print(sha256(content).hexdigest())
